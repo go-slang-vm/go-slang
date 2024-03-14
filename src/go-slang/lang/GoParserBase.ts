283741ccba620840c17af45230eb8fdef16c2a70
bryans17 <77219972+bryans17@users.noreply.github.com>
2024-03-14 18:34:00 +0800

    Add parser for a small subset of Go

---
+++ src/go-slang/lang/GoParserBase.ts
@@ -0,0 +1,10 @@
+import { Parser } from "antlr4ts";
+
+import { GoLexer }  from "./GoLexer";
+
+export abstract class GoParserBase extends Parser {
+  closingBracket(): boolean {
+    const la = this._input.LA(1);
+    return la === GoLexer.R_PAREN || la === GoLexer.R_CURLY;
+  }
+}
