55543594c79fbf5b4c09c6c3956410c5ec9418e7
seancze <60085582+seancze@users.noreply.github.com>
2024-03-22 13:27:27 +0800

    refactor: shift goRunner into go-slang/ directory

--- src/runner/index.ts
+++ src/runner/index.ts
@@ -1,5 +1,5 @@
 export * from './fullJSRunner'
 export * from './htmlRunner'
 export * from './sourceRunner'
-export * from './goRunner'
+export * from '../go-slang/runner/goRunner'
 export * from './utils'
