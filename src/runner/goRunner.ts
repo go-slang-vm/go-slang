3068f4acbeb419ae6d428986d6bcb5436706cd24
seancze <60085582+seancze@users.noreply.github.com>
2024-03-22 09:48:05 +0800

    feat: add goRunner (connecting compiler and vm together)

--- src/runner/goRunner.ts
+++ src/runner/goRunner.ts
@@ -1,47 +1,26 @@
-import { IOptions } from '..'
-import { ASTNode } from '../go-slang/ast/AST'
 import { parse } from '../go-slang/parser/parser'
-import { Context, SourceError, Variant } from '../types'
 import { resolvedErrorPromise } from './utils'
+import { IOptions, Result } from '..'
+import { ASTNode } from '../go-slang/ast/AST'
+import { Context, RecursivePartial } from '../types'
+import { compile_program } from '../go-slang/compiler/compiler'
+import { VM } from '../go-slang/vm'
 
-const DEFAULT_SOURCE_OPTIONS: Readonly<IOptions> = {
-  scheduler: 'async',
-  steps: 1000,
-  stepLimit: -1,
-  executionMethod: 'auto',
-  variant: Variant.DEFAULT,
-  originalMaxExecTime: 1000,
-  useSubst: false,
-  isPrelude: false,
-  throwInfiniteLoops: true,
-  envSteps: -1,
-  importOptions: {
-    wrapSourceModules: true,
-    checkImports: true,
-    loadTabs: true
+export async function goRunner(
+  code: string,
+  context: Context,
+  options: RecursivePartial<IOptions> = {}
+): Promise<Result> {
+  const program: ASTNode | undefined = parse(code)
+  if (!program) {
+    return resolvedErrorPromise
   }
-}
+  const compiledProgram: any[] = compile_program(program)
+  const vm = new VM(1500)
 
-export function goRunner(code: string, context: Context, options: Partial<IOptions> = {}): any {
-  // const theOptions: IOptions = { ...DEFAULT_SOURCE_OPTIONS, ...options }
-  context.variant = DEFAULT_SOURCE_OPTIONS.variant
-  context.errors = []
-
-  try {
-    const program: ASTNode | undefined = parse(code)
-    if (!program) {
-      return resolvedErrorPromise
-    }
-    console.dir({ program }, { depth: null })
-    return undefined
-    // const it = evaluate(program, context)
-    // const scheduler: Scheduler = new PreemptiveScheduler(theOptions.steps)
-    // return scheduler.run(it, context)
-  } catch (e) {
-    context.errors.push(e as SourceError)
-    context.runtime.environments = context.runtime.environments.slice(
-      -context.numberOfOuterEnvironments
-    )
-    return Promise.resolve({ status: 'error', error: e as SourceError })
-  }
+  return Promise.resolve({
+    status: 'finished',
+    context,
+    value: vm.run(compiledProgram)
+  })
 }
