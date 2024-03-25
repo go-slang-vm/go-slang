9856164e1e98d700ebc986100ed81df638281181
seancze <60085582+seancze@users.noreply.github.com>
2024-03-25 21:49:54 +0800

    test: add another block scope variable declaration test

--- src/go-slang/compiler/__tests__/compiler.test.ts
+++ src/go-slang/compiler/__tests__/compiler.test.ts
@@ -1,6 +1,6 @@
-import { ASTNode } from '../ast/AST'
-import { compile_program } from '../compiler/compiler'
-import { parse } from '../parser/parser'
+import { ASTNode } from '../../ast/AST'
+import { compile_program } from '../../compiler/compiler'
+import { parse } from '../../parser/parser'
 
 describe('Basic compiler test', () => {
   test('basic variable declaration in a new block scope', async () => {
@@ -40,13 +40,49 @@ describe('Basic compiler test', () => {
     ]
 
     const inputAst: ASTNode = parse(program)
-    //console.dir(inputAst, {depth: 100});
-    //try {
     const outputInstr: any[] = compile_program(inputAst)
-    //console.log(JSON.stringify(outputInstr));
-    //} catch(e) {
-    //console.log("error: " + e);
-    //}
+    expect(outputInstr).toStrictEqual(expectedInstr)
+  })
+
+  test('basic variable declaration in a new block scope 2', async () => {
+    const program = `
+      func main() (int) {
+        var y int = 1
+        {
+          y int := 2
+          return y
+        }
+        
+      }`
+
+    const expectedInstr = [
+      { tag: 'ENTER_SCOPE', num: 1 },
+      { tag: 'LDF', arity: 0, addr: 3 },
+      { tag: 'GOTO', addr: 17 },
+      { tag: 'ENTER_SCOPE', num: 1 },
+      { tag: 'LDC', val: 1 },
+      { tag: 'ASSIGN', pos: [4, 0] },
+      { tag: 'POP' },
+      { tag: 'ENTER_SCOPE', num: 1 },
+      { tag: 'LDC', val: 2 },
+      { tag: 'ASSIGN', pos: [5, 0] },
+      { tag: 'POP' },
+      { tag: 'LD', sym: 'y', pos: [5, 0] },
+      { tag: 'RESET' },
+      { tag: 'EXIT_SCOPE' },
+      { tag: 'EXIT_SCOPE' },
+      { tag: 'LDC', val: undefined },
+      { tag: 'RESET' },
+      { tag: 'ASSIGN', pos: [2, 0] },
+      { tag: 'POP' },
+      { tag: 'LD', sym: 'main', pos: [2, 0] },
+      { tag: 'CALL', arity: 0 },
+      { tag: 'EXIT_SCOPE' },
+      { tag: 'DONE' }
+    ]
+
+    const inputAst: ASTNode = parse(program)
+    const outputInstr: any[] = compile_program(inputAst)
     expect(outputInstr).toStrictEqual(expectedInstr)
   })
 
