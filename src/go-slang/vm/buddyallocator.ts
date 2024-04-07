473aa97fe0c0819303132bfc42b2737673659c6d
bryans17 <77219972+bryans17@users.noreply.github.com>
2024-04-07 15:04:51 +0800

    refactor buddy allocator

--- src/go-slang/vm/buddyallocator.ts
+++ src/go-slang/vm/buddyallocator.ts
@@ -1,6 +1,6 @@
 export class BuddyAllocator {
     _data: ArrayBuffer
-    FreeListMap: Map<number, number[]>
+    FreeList: Map<number, number[]>
     smallestblocksize = 1
     largestblocksize: number
     size: number
@@ -9,56 +9,59 @@ export class BuddyAllocator {
         this.size = 1 << sizeInPowerOf2
         this.largestblocksize = 1 << sizeInPowerOf2
         this._data = new ArrayBuffer(this.size)
-        this.FreeListMap = new Map<number, number[]>()
-        const initialList: number[] = [0]
-        this.FreeListMap.set(this.size, initialList)
+        this.initializeFreeList()
+    }
+
+    initializeFreeList() {
+        this.FreeList = new Map<number, number[]>()
+        for(let i = 1; i <= this.size; i *= 2) {
+            this.FreeList.set(i, [])
+        }
+        this.FreeList.get(this.size)?.push(0)
+    }
+
+    hasBlockOfSize(blockSizeNeeded: number): boolean {
+        return this.FreeList.get(blockSizeNeeded)!.length > 0
     }
 
     allocate(size: number): DataView | null {
         const smallestBlockNeeded = 1 << Math.ceil(Math.log2(size))
-        if (this.FreeListMap.has(smallestBlockNeeded)) {
-            const addr = this.FreeListMap.get(smallestBlockNeeded)![0]
-            this.FreeListMap.get(smallestBlockNeeded)!.shift()
+        if (this.hasBlockOfSize(smallestBlockNeeded)) {
+            const addr = this.FreeList.get(smallestBlockNeeded)![0]
+            this.FreeList.get(smallestBlockNeeded)!.shift()
             return new DataView(this._data, addr, smallestBlockNeeded)
         } else {
             this.breakBlocksIntoLargerPieces(smallestBlockNeeded)
-            if (!this.FreeListMap.has(smallestBlockNeeded)) {
+            if (!this.hasBlockOfSize(smallestBlockNeeded)) {
                 return null
             }
 
-            const addr = this.FreeListMap.get(smallestBlockNeeded)![0]
-            this.FreeListMap.get(smallestBlockNeeded)!.shift()
+            const addr = this.FreeList.get(smallestBlockNeeded)![0]
+            this.FreeList.get(smallestBlockNeeded)!.shift()
             return new DataView(this._data, addr, smallestBlockNeeded)
         }
     }
 
     breakBlocksIntoLargerPieces(startingBlock: number) {
         let currBlock = startingBlock
-        console.log("largest: " + this.largestblocksize)
-        while(currBlock < this.largestblocksize && !this.FreeListMap.has(currBlock)) {
+        // console.log("largest: " + this.largestblocksize)
+        while(currBlock < this.largestblocksize && !this.hasBlockOfSize(currBlock)) {
             currBlock *= 2
         }
 
-        console.log("currBlock: " + currBlock)
+        // console.log("currBlock: " + currBlock)
 
-        if (!this.FreeListMap.has(currBlock)) {
+        if (!this.hasBlockOfSize(currBlock)) {
             // unable to split
             return;
         }
 
         while(currBlock != startingBlock) {
-            const addr = this.FreeListMap.get(currBlock)![0]
-            this.FreeListMap.get(currBlock)?.shift()
-            if(this.FreeListMap.get(currBlock)?.length === 0) {
-                this.FreeListMap.delete(currBlock)
-            }
+            const addr = this.FreeList.get(currBlock)![0]
+            this.FreeList.get(currBlock)!.shift()
 
-            if (this.FreeListMap.get(currBlock/2) === undefined) {
-                this.FreeListMap.set(currBlock/2, [])
-            }
-
-            this.FreeListMap.get(currBlock/2)?.push(addr)
-            this.FreeListMap.get(currBlock/2)?.push(addr + currBlock/2)
+            this.FreeList.get(currBlock/2)!.push(addr)
+            this.FreeList.get(currBlock/2)!.push(addr + currBlock/2)
 
             currBlock /= 2
         }
@@ -66,36 +69,25 @@ export class BuddyAllocator {
 
     free(addr: number, size: number) {
         const smallestBlockNeeded = 1 << Math.ceil(Math.log2(size)) 
-        if (!this.FreeListMap.has(smallestBlockNeeded)) {
-            this.FreeListMap.set(smallestBlockNeeded, [addr])
-            return;
-        }
 
         let currBlock = smallestBlockNeeded
 
         // Merge adjacent free blocks if possible
         while (currBlock < this.largestblocksize) {
             const buddy = addr ^ currBlock; // XOR to find buddy
-            const buddyIndex = this.FreeListMap.get(currBlock)?.indexOf(buddy);
-            console.log("currBlock: " + currBlock + " buddy: " + buddy + " buddy index: " + buddyIndex)
+            const buddyIndex = this.FreeList.get(currBlock)!.indexOf(buddy);
+
+            // console.log("currBlock: " + currBlock + " buddy: " + buddy + " buddy index: " + buddyIndex)
 
             if (buddyIndex === -1 || buddyIndex === undefined) break;
 
-            this.FreeListMap.get(currBlock)?.splice(buddyIndex!, 1); // Remove buddy from free list
-
-            if (this.FreeListMap.get(currBlock)?.length === 0) {
-                this.FreeListMap.delete(currBlock)
-            }
+            this.FreeList.get(currBlock)!.splice(buddyIndex!, 1); // Remove buddy from free list
 
             addr = Math.min(addr, buddy);
             currBlock *= 2;
         }
 
-        if(!this.FreeListMap.has(currBlock)) {
-            this.FreeListMap.set(currBlock, [])
-        }
-
-        console.log("inserting into block size: " + currBlock + " addr is: " + addr)
-        this.FreeListMap.get(currBlock)?.push(addr)
+        // console.log("inserting into block size: " + currBlock + " addr is: " + addr)
+        this.FreeList.get(currBlock)!.push(addr)
     }
 }
