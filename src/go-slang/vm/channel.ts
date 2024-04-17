374b56b6707b995f81024800a5beac54416273a2
seancze <60085582+seancze@users.noreply.github.com>
2024-04-17 20:04:16 +0800

    refactor: add comment to explicitly define pop method of low level queue

--- src/go-slang/vm/channel.ts
+++ src/go-slang/vm/channel.ts
@@ -1,46 +1,47 @@
-import { LowLevelQueue } from "./lowlevelqueue"
-import { Thread } from "./thread"
+import { LowLevelQueue } from './lowlevelqueue'
+import { Thread } from './thread'
 
 export class Channel {
-    idx: number
-    capacity: number
-    // this contains the heap addresses of the items to be read out
-    // items: number[]
-    items: LowLevelQueue
-    // this contains the Thread objects that blocked on a send call
-    sendBlockedQueue: Thread[]
-    // this contains the Thread objects that blocked on a recv call
-    recvBlockedQueue: Thread[]
-    constructor(idx: number, capacity: number, buffer: DataView) {
-        this.idx = idx
-        this.capacity = capacity
-        this.items = new LowLevelQueue(capacity, buffer)
-        this.sendBlockedQueue = []
-        this.recvBlockedQueue = []
-    }
+  idx: number
+  capacity: number
+  // this contains the heap addresses of the items to be read out
+  // items: number[]
+  items: LowLevelQueue
+  // this contains the Thread objects that blocked on a send call
+  sendBlockedQueue: Thread[]
+  // this contains the Thread objects that blocked on a recv call
+  recvBlockedQueue: Thread[]
+  constructor(idx: number, capacity: number, buffer: DataView) {
+    this.idx = idx
+    this.capacity = capacity
+    this.items = new LowLevelQueue(capacity, buffer)
+    this.sendBlockedQueue = []
+    this.recvBlockedQueue = []
+  }
 
-    clear() {
-        this.items.clear()
-        this.sendBlockedQueue = []
-        this.recvBlockedQueue = []
-    }
+  clear() {
+    this.items.clear()
+    this.sendBlockedQueue = []
+    this.recvBlockedQueue = []
+  }
 
-    pushToItemQueue(item: number) {
-        this.items.push(item)
-    }
+  pushToItemQueue(item: number) {
+    this.items.push(item)
+  }
 
-    popFromItemQueue(): number {
-        if (this.items.getSize() == 0) {
-            throw new Error("Channel is empty should not have been popped")
-        }
-        return this.items.pop()
+  popFromItemQueue(): number {
+    if (this.items.getSize() == 0) {
+      throw new Error('Channel is empty should not have been popped')
     }
+    // NOTE: this pop method removes the item from the FRONT of the queue; it is not the same as the pop method in the JS array class
+    return this.items.pop()
+  }
 
-    getSendQueue(): Thread[] {
-        return this.sendBlockedQueue
-    }
+  getSendQueue(): Thread[] {
+    return this.sendBlockedQueue
+  }
 
-    getRecvQueue(): Thread[] {
-        return this.recvBlockedQueue
-    }
+  getRecvQueue(): Thread[] {
+    return this.recvBlockedQueue
+  }
 }
